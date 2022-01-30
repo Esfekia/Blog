27bb9e67a4286de55fcd57db0874f101a9b1c60c
esfekia <sertac@gmail.com>
2022-01-30 14:49:52 -0500

    Added user ownership for blogposts, updated database and assigned already posted blogposts to admin's ownership, verified in shell.

--- blogs/models.py
+++ blogs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -8,6 +9,7 @@ class BlogPost(models.Model):
     blog_title = models.CharField(max_length=50)
     text = models.CharField(max_length=1000)
     date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         """Return a single representation of the model."""
